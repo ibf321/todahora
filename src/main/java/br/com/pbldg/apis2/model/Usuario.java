899471e776b04c53d2d8a8b2eace89d7307e5dfb
Pablo Diego <pablodcardoso@gmail.com>
2015-09-09 20:51:15 -0300

    JPA connection

--- src/main/java/br/com/pbldg/apis2/model/Usuario.java
+++ src/main/java/br/com/pbldg/apis2/model/Usuario.java
@@ -8,22 +8,22 @@ import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
-//@Entity
-//@Table(name="TB_USUARIO", schema="apis1")
-//@SequenceGenerator(name = "UsuarioSequence", sequenceName = "SQ_ID_USUARIO", allocationSize = 1)
+@Entity
+@Table(name="TB_USUARIO", schema="apis2")
+@SequenceGenerator(name = "UsuarioSequence", sequenceName = "SQ_ID_USUARIO", allocationSize = 1)
 public class Usuario {
 	
-//	@Id
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "UsuarioSequence")
-//	private Long id;
-//	
-//    @Column(name = "LOGIN", length = 30, nullable = false)
-//    private String login;
-//    
-//    @Column(name = "SENHA", nullable = false)
-//    private String senha;
-//
-//    @Column(name = "EMAIL", length = 50, nullable = false, unique = true)
-//    private String email;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "UsuarioSequence")
+	private Long id;
+	
+    @Column(name = "LOGIN", length = 30, nullable = false)
+    private String login;
+    
+    @Column(name = "SENHA", nullable = false)
+    private String senha;
+
+    @Column(name = "EMAIL", length = 50, nullable = false, unique = true)
+    private String email;
 
 }
