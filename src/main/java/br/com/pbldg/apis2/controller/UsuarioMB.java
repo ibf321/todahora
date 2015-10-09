b0f9a7b71c856249d7c918f6def8519edd84a076
Pablo Diego <pablodcardoso@gmail.com>
2015-10-09 18:48:17 -0300

    Iniciando JSF

--- src/main/java/br/com/pbldg/apis2/controller/UsuarioMB.java
+++ src/main/java/br/com/pbldg/apis2/controller/UsuarioMB.java
@@ -1,16 +1,28 @@
 package br.com.pbldg.apis2.controller;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
-@ManagedBean
-@ViewScoped
+import br.com.pbldg.apis2.dao.JPAUtil;
+import br.com.pbldg.apis2.model.Usuario;
+
+@ManagedBean(name="usuarioMB")
+@RequestScoped
 public class UsuarioMB implements Serializable {
 	
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5202799275364383072L;
+	
 	private String nome;
+	
 
 	public String getNome() {
 		return nome;
@@ -20,6 +32,15 @@ public class UsuarioMB implements Serializable {
 		this.nome = nome;
 	}
 	
-	
+	public void salvar() {
+		System.out.println("Foi!");
+		
+//		EntityManager em = JPAUtil.getEntityManager();
+//		Query q = em.createQuery("select a from Usuario a", Usuario.class);
+//		List<Usuario> usuarios = q.getResultList();
+//		for (Usuario usuario : usuarios) {
+//			System.out.println(usuario.getLogin());
+//		}
+	}
 	
 }
---
+++ src/main/java/br/com/pbldg/apis2/dao/JPAUtil.java
@@ -0,0 +1,15 @@
+package br.com.pbldg.apis2.dao;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil {
+	
+	private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("todahora");
+	
+	public static EntityManager getEntityManager(){
+		return emf.createEntityManager();
+	}
+
+}
